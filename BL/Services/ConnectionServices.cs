0aa78da5ddc2ab7a57a13ea43b98a66e94d2087c
martinbednar <martin.bedn20@gmail.com>
2024-12-16 14:29:11 +0100

    BL: Add services.

---
+++ BL/Services/ConnectionServices.cs
@@ -0,0 +1,25 @@
+﻿using OpcUaClient;
+using OpcUaClient.ConcreteOpcUaClients;
+
+namespace BL.Services
+{
+    public class ConnectionServices
+    {
+        private async Task SetLiveBitAsync(AbstractOpcUaClient opcUaClient, bool value)
+        {
+            await opcUaClient.WriteValueAsync(opcUaClient.LiveBitNode, value);
+        }
+
+        public async Task InvertLiveBitAsync(AbstractOpcUaClient opcUaClient)
+        {
+            bool liveBitCurrectValue = await opcUaClient.ReadValueAsync(opcUaClient.LiveBitNode, new bool());
+            await SetLiveBitAsync(opcUaClient, !liveBitCurrectValue);
+        }
+
+        public async void InvertLiveBitsAsync()
+        {
+            await InvertLiveBitAsync(PressOpcUaClient.Instance);
+            await InvertLiveBitAsync(ScrewOpcUaClient.Instance);
+        }
+    }
+}
