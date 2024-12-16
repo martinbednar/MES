8cdbe88393cc12039ba62e6a4aebed5c626431fe
martinbednar <martin.bedn20@gmail.com>
2024-12-16 14:35:21 +0100

    BL: Add daemon to run OpcUa clients.

---
+++ BL/Daemons/Helpers/MonitoredItemsHelper.cs
@@ -0,0 +1,54 @@
+﻿using BL.Mappers;
+using OpcUaClient.ConcreteOpcUaClients;
+using OpcUaClient.Models;
+
+namespace OpcUaClient.Helpers
+{
+    internal class MonitoredItemsHelper
+    {
+        internal List<OpcUaMonitoredItem> GetOpcUaSubscriptions(AbstractOpcUaClient opcUaClient)
+        {
+            MonitoredItemsMapper monitoredItemsMapper = new MonitoredItemsMapper();
+
+            List<OpcUaMonitoredItem> opcUaMonitoredItems = new List<OpcUaMonitoredItem>();
+
+            foreach (string monitoredItem in opcUaClient.MonitoredItems)
+            {
+                opcUaMonitoredItems.Add(new OpcUaMonitoredItem()
+                {
+                    NodeId = monitoredItem,
+                    Handler = monitoredItemsMapper.GetNewDataReadyHandler(monitoredItem).NewDataReady
+                });
+            }
+            return opcUaMonitoredItems;
+        }
+
+        internal void RegisterMonitoredItems(AbstractOpcUaClient opcUaClient, List<OpcUaMonitoredItem> opcUaMonitoredItems)
+        {
+            while (true)
+            {
+                try
+                {
+                    opcUaClient.AddListeners(opcUaMonitoredItems);
+                    break; // Exit the loop when listeners successfully added.
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine("OPC UA Client: Failed to register monitored items.");
+                }
+            }
+        }
+
+        internal async Task AddHandlersToMonitoredItems()
+        {
+            List<OpcUaMonitoredItem> pressOpcUaMonitoredItems = GetOpcUaSubscriptions(PressOpcUaClient.Instance);
+            List<OpcUaMonitoredItem> screwOpcUaMonitoredItems = GetOpcUaSubscriptions(ScrewOpcUaClient.Instance);
+
+            await Task.Delay(1000);
+
+            RegisterMonitoredItems(PressOpcUaClient.Instance, pressOpcUaMonitoredItems);
+            RegisterMonitoredItems(ScrewOpcUaClient.Instance, screwOpcUaMonitoredItems);
+        }
+    }
+}
---
+++ BL/Daemons/OpcUaClientDaemon.cs
@@ -0,0 +1,38 @@
+﻿using BL.Services;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using OpcUaClient.Helpers;
+
+namespace BL.Daemons
+{
+    public class OpcUaClientDaemon : BackgroundService
+    {
+        private readonly ILogger<OpcUaClientDaemon> _logger;
+
+        public OpcUaClientDaemon(ILogger<OpcUaClientDaemon> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            await _startOpcUaClientDaemon(stoppingToken);
+        }
+
+        private async Task _startOpcUaClientDaemon(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("OPC UA Daemon routine started.");
+
+            MonitoredItemsHelper monitoredItemsHelper = new MonitoredItemsHelper();
+            await monitoredItemsHelper.AddHandlersToMonitoredItems();
+
+            ConnectionServices connectionServices = new ConnectionServices();
+
+            while (true)
+            {
+                connectionServices.InvertLiveBitsAsync();
+                await Task.Delay(2000, cancellationToken);
+            }
+        }
+    }
+}
