a35ed1575242c980bfd859a60786528de7673373
martinbednar <martin.bedn20@gmail.com>
2025-02-07 14:29:19 +0100

    Enable CORS.

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -35,6 +35,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors(options => options
+    .AllowAnyOrigin()
+    .AllowAnyHeader()
+    .AllowAnyMethod()
+);
+
 app.UseHttpsRedirection();
 
 app.MapControllers();
